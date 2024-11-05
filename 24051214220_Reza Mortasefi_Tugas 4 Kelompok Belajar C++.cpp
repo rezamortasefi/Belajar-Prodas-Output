4a1bb8e12dcbcdf718b9d18c7bd14df756fcd116
rezamortasefi <rezamortasefi@gmail.com>
2024-11-05 18:09:52 +0700

    Add files via upload

---
+++ 24051214220_Reza Mortasefi_Tugas 4 Kelompok Belajar C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	string nama;
+	cout << "Tuliskan nama anda : ";
+	cin >> nama;
+	
+	cout << "Hii " <<  nama;
+	cout << " Selamat datang di UNESA!" << endl;
+	
+	return 0;
+}
